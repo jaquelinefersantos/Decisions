1feedc22be1f80dd6ec9fab0825670d184f412ae
JAQUELINE SANTOS <jackfersantos@hotmail.com>
2018-08-30 17:56:23 -0300

    Aula4

--- Decisions/Program.cs
+++ Decisions/Program.cs
@@ -9,7 +9,47 @@ namespace Decisions
     class Program
     {
         static void Main(string[] args)
-        {
+        { /*
+            Console.WriteLine("Bob's Big Giveaway");
+            Console.WriteLine("Choose a door: 1,2 or 3:");
+            string userValue = Console.ReadLine();
+            string message = "";
+
+            if (userValue == "1") ;
+            {
+                message = "You won a new car!";   
+             else if (userValue == "2")
+                message = "You won a new boat!";
+             else if (userValue == "3") ;
+                message = "You won a new cat!";      
+             else 
+            {
+                message = "Sorry, we didn't understand. \n";
+                    //message = message + "You lose.";
+                message += "You Lose";
+                }
+
+                Console.WriteLine(message);
+                Console.ReadLine();
+                */
+
+            Console.WriteLine("Bob's Big Giveaway");
+            Console.WriteLine("Choose a door: 1,2 or 3:");
+            string userValue = Console.ReadLine();
+
+            string message = (userValue == "1") ? "boat" : "strand of lint";
+
+            //Console.Write("you won a");
+            //Console.Write(message);
+            //Console.Write(".");
+
+            //Console.WriteLine ("You won a {0}.", message);
+            Console.WriteLine("You entered: {0}. therefore you won a {1}.", userValue, message);
+
+            Console.ReadLine();
+                        
+            
+            }
         }
-    }
-}
+    } 
+
